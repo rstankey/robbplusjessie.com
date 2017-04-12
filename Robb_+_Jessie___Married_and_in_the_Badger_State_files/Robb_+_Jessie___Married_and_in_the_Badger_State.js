ad60d572d90985232b9fcd9a1a7833c69c3c1c28
rstankey <robb@CRC-Lividans.local>
2017-04-12 10:55:35 -0500

    robbplusjessie
    Personal website for Robb Stankey and Jessie Stankey

---
+++ Robb_+_Jessie___Married_and_in_the_Badger_State_files/Robb_+_Jessie___Married_and_in_the_Badger_State.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.2 local-build-20110209
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Robb_+_Jessie___Married_and_in_the_Badger_State_files/Robb_+_Jessie___Married_and_in_the_Badger_StateMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
